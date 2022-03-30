7e996d9722235529aaa0c8b86382b6d3ab2a9dd0
weinymarchelina <weinymarchelina31@gmail.com>
2022-03-30 21:34:35 +0800

    add pwa

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
-module.exports = {
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+
+module.exports = withPWA({
   reactStrictMode: true,
-}
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    // disable: process.env.NODE_ENV === "development",
+    runtimeCaching,
+  },
+});
